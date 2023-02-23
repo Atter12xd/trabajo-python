c989a1f09dc188511f03323547f4a7be08d39d1a
Atter12xd <1485275@senati.pe>
2023-02-23 11:08:57 -0500

    fin

--- python-trabajo.py
+++ python-trabajo.py
@@ -1,2 +1,9 @@
-# ahora vamos a proceder a multiplicar y dividir las variables
-# para multiplicar en python se utiliza esto "*" para sumar utilizaremos esto "+"
+
+l = 10
+x = 8 
+
+multiplicar = l * x 
+Suma = l + x
+
+print("la suma es", Suma)
+print("La multiplicacion es", multiplicar)
