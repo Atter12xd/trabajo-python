56b3eda62a54657a4535d3fb09903bf2e5b67b2a
Atter12xd <1485275@senati.pe>
2023-02-23 11:05:50 -0500

    pasos para hacer el trabajo

--- python-trabajo.py
+++ python-trabajo.py
@@ -1,4 +1,2 @@
-# Ahora vamos a poner valor a nuestras variables 
-l = 10
-x = 8
-
+# ahora vamos a proceder a multiplicar y dividir las variables
+# para multiplicar en python se utiliza esto "*" para sumar utilizaremos esto "+"
